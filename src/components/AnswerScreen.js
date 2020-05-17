f6e944d6147136ddf8adfe7daba057d31ad03a5f
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2020-05-17 18:09:27 -0500

    basic project directory structure in place

---
+++ src/components/AnswerScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../css/AnswerScreen.css'
+
+class AnswerScreen extends React.Component {
+    render() {
+        return (
+            <div className='answer-screen'>
+
+            </div>
+        )
+    }
+}
+
+export default AnswerScreen
---
+++ src/components/Game.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../css/Game.css'
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className='Game'>
+
+            </div>
+        )
+    }
+}
+
+export default Game
